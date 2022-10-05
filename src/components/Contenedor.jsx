770127e031bbfc89a6a36997d6abdf237e5bd67f
Zuko <jose.apep570@gmail.com>
2022-10-05 07:53:18 -0500

    [ADD] componente Button agregado al contenedor

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,15 +1,17 @@
 import "../assets/styles/contenedor.css";
 import Email from "./Email";
 import Password from "./Password";
+import Button from "./Button";
 
 function Contenedor() {
   return (
     <>
       <center>
         <div className="wrapper">
-            <Email />
-            <Password />
-            </div>
+          <Email />
+          <Password />
+          <Button />
+        </div>
       </center>
     </>
   );
