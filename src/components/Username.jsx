98d4f05768aaf2786cbfe6abfde507ea242a47e0
Zuko <jose.apep570@gmail.com>
2022-10-11 09:30:02 -0500

    [UPDATE] User component

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/Fields.css'
 function Username() {
     return ( 
         <div className="field">
-            <label>Last Name: </label>
+            <label>Username: </label>
             <input type="text" id="usernme" />  
         </div>
      );
